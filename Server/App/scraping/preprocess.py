1f108086353061dcc78a2f04754ad73db77a248b
PatelSnehV <snehpatel1233@gmail.com>
2025-10-20 18:58:00 +0530

    scraping-data-cleaning

---
+++ Server/App/scraping/preprocess.py
@@ -0,0 +1,25 @@
+import re
+from typing import List,Dict
+def clean_text(text: str):
+    """
+    Removes extra spaces
+    """
+    return re.sub(r"\s+"," ",text).strip()
+
+def chuncking(content: Dict , chunk_size: int=500) -> List[Dict]:
+    cleaned_content = clean_text(content["content"])
+    words = cleaned_content.split()
+    chunks =[]
+    
+    for i in range(0,len(words),chunk_size):
+        chunk_words = words[i : i + chunk_size]
+        chunk_text = " ".join(chunk_words)
+        chunks.append(
+            {
+                "url" : content["url"],
+                "title" :  content["title"],
+                "chunk_id" : i,
+                "content" : chunk_text
+            }
+        )
+        return chunks
--- Server/App/scraping/scraper.py
+++ Server/App/scraping/scraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup ,XMLParsedAsHTMLWarning
 import warnings
 import requests
 from pdfminer.high_level import extract_text as pdf_extract_text
-from io import IOBase
+from io import BytesIO
 
 warnings.filterwarnings("ignore",category=XMLParsedAsHTMLWarning)
 def extract_text_from_url(url :str):
@@ -14,12 +14,12 @@ def extract_text_from_url(url :str):
         print(f"Error in fetching the URL:{url} , error:{e} ")
         return None
        
-    content_type = response.headers.get("Content-Type", "").lower() # To know the content type of scraped data so that we can diffrentiate the method of parcing diffrent types.
+    content_type = response.headers.get("Content-Type").lower() # To know the content type of scraped data so that we can diffrentiate the method of parcing diffrent types.
     
-    #Handle PDF
-    if "application/pdf" in content_type and url.endswith(".pdf"):
+    # Handle PDF
+    if "application/pdf" in content_type or url.endswith(".pdf"):
         try:
-            pdf_text =pdf_extract_text(IOBase(response.content))
+            pdf_text =pdf_extract_text(BytesIO(response.content))
             return {
                 "url": url,
                 "title": url.split("/")[-1],#last name afer "/"
@@ -29,5 +29,51 @@ def extract_text_from_url(url :str):
             print(f"Error in fetching this pdf of url : {url} error is {e}")
             return None
 
+    # Handle XML(sitemaps and RSS)
+    elif "xml" in content_type or url.endswith(".xml") or url.endswith(".rss"):
+        try:
+            soup = BeautifulSoup(response.text,"xml")
+        except Exception as e:
+            print(f"fallback for HTML conten of {url}")
+            soup = BeautifulSoup(response.text,"html.parser")
+            
+        title = (
+            soup.title.string.strip()
+            if soup.title and soup.title.string
+            else "No Title"
+        )
+        text = soup.get_text(separator=" " ,strip=True)
+        return {"url" : url ,"title" : title,"content":text}
+    # Handle HTML  
+    elif "text/html" in content_type or url.endswith(".html"):
+        try:
+            soup = BeautifulSoup(response.text,"html.parser")
+            
+            title =(
+                    soup.title.string.strip()
+                    if soup.title or soup.title.string
+                    else
+                    "No Title")
+            
+            # cleaning the html
+            for script in soup(["script","style","noscript"]):
+                script.extract()
+            
+            text = soup.get_text(strip=True,separator=" ")
+            
+            if len(text) < 50 or not text:
+                return None
+            
+            return {"url" : url ,"title" : title,"content":text}        
+            
+        except Exception as e:
+            print(f"Error in HTML parsing the URL:{url} , error:{e} ")
+            return None
+        
+    else:
+            print(f"There is not proper Formate to scrap.")
+            return None
+      
 
+    
 
